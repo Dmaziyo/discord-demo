119d0eba32eef1c81c0ac0de25a22da89d4bf2fd
liangjunqi <247948681@qq.com>
2023-12-12 14:01:51 +0800

    feat: dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { ClerkProvider } from '@clerk/nextjs'
 import { Inter } from 'next/font/google'
 import './globals.css'
 
+import { ThemeProvider } from '@/components/theme-provider'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -14,7 +15,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+            {children}
+          </ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
   )
--- components/navigation/navigation-action.tsx
+++ components/navigation/navigation-action.tsx
@@ -4,8 +4,8 @@ import ActionToolTip from '@/components/action-tooltip'
 const NavigationAction = () => {
   return (
     <ActionToolTip label="Add a server" side="right" align="start">
-      <button className="group flex items-center justify-center">
-        <div className="flex mx-3 justify-center items-center h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] group-hover:bg-emerald-500  transition-all">
+      <button className="group flex items-center justify-center ">
+        <div className="dark:bg-neutral-700 flex mx-3 justify-center items-center h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] group-hover:bg-emerald-500  transition-all">
           <Plus size={25} className="text-emerald-500 transition group-hover:text-white "></Plus>
         </div>
       </button>
