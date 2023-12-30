41b90646085b97a348dcbbe85666f749653a097b
liangjunqi <247948681@qq.com>
2023-12-30 13:48:20 +0800

    fix(component):user-avatar

--- components/user-avator.tsx
+++ components/user-avator.tsx
@@ -1,13 +1,15 @@
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { cn } from '@/lib/utils'
 
 interface UserAvatarProps {
   src?: string
-  alt?: string
+  alt?: string,
+  className?:string
 }
 
-const UserAvatar = ({ src, alt }: UserAvatarProps) => {
+const UserAvatar = ({ src, alt,className }: UserAvatarProps) => {
   return (
-    <Avatar>
+    <Avatar className={cn("h-7 w-7 md:h-10 md:w-10", className)}>
       <AvatarImage src={src} />
       <AvatarFallback>{alt?.slice(0,2)}</AvatarFallback>
     </Avatar>
