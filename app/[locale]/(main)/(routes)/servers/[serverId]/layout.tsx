746c0484440e26555c5316cc50eec3134d5664c0
liangjunqi <247948681@qq.com>
2024-01-15 12:20:56 +0800

    style:ui-alignment

--- app/[locale]/(main)/(routes)/servers/[serverId]/layout.tsx
+++ app/[locale]/(main)/(routes)/servers/[serverId]/layout.tsx
@@ -12,7 +12,7 @@ const ServerPageLayout = ({
 }) => {
   return (
     <div className="h-full">
-      <div className="hidden md:flex flex-col fixed w-60 inset-y-0  z-20 py-3 bg-[#F2F3F5] dark:bg-[#2B2D31]">
+      <div className="hidden md:flex flex-col fixed w-60 inset-y-0  z-20 bg-[#F2F3F5] dark:bg-[#2B2D31]">
         <ServerSideBar serverId={params.serverId} locale={params.locale} />
       </div>
       <div className="pl-60 h-full">{children}</div>
